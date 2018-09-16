1a5cf7716b1e85eaeaf39783e012957fc05f7550
Collin Smith <collin@collinsmith.me>
2018-09-15 23:15:19 -0500

    Add portfolio page

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PortfolioPage({ data }) {
+  return (
+    <div className="group">
+      <section id="contact-primary">
+        <p>Under construction.</p>
+      </section>
+    </div>
+  );
+}
