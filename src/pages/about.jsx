aa2d6982de4dee492c55701062cf05e992f0869a
Collin Smith <collin@collinsmith.me>
2018-11-17 23:06:16 -0600

    Remove gatsby-link

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "gatsby-link";
+import { Link } from "gatsby"
 import ProfilePhoto from "../assets/img/profile_pic.jpeg";
 import Layout from "../components/layout";
 
